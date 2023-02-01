200391a9b3968ccfdaf80044f7ce89ade4be1c7e
parody9c@gmail.com <parody9c@gmail.com>
2023-02-01 17:54:28 +0900

    [ADD] tensorflow/movenet module && new component for movenet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">pose-net</router-link> |
+    <router-link to="/move">move-net</router-link> |
+    <router-link to="/about">kakao-api</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/MoveView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/yuzLogo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <MoveNetComponents></MoveNetComponents>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+import MoveNetComponents from '@/components/MoveNetComponents.vue';
+
+  export default {
+    name: 'MoveView',
+    components: {
+      MoveNetComponents,
+    }
+  }
+  </script>
+  
